3bf3c7f5140ae23724223959c20ee1c4e8808546
Jodie Wiggins <jodiewiggins18@gmail.com>
2019-02-04 06:44:11 -0500

    refining taxon list

--- R/historyofearth.R
+++ R/historyofearth.R
@@ -95,8 +95,17 @@ GetAgeDF <- function() {
 #' @return vector of names
 #' @export
 GetTaxa <- function() {
-  return(c("Attercopus", "Gorilla", "Panthera", "Homo", "Tyto", "Dromaius", "Aedes", "Solenopsis", "Caretta", "Crocodylus", "Solanum", "Prunus", "Rosa", "Climacograptus", "Anomalocaris", "Dunkleosteus", "Halysites", "Histiodella", "Agathiceras", "Archaeopteryx", "Juramaia", "Hylonomus", "Elginerpeton", "Rhyniognatha", "Aculeisporites", "Canadaspis", "Arandaspis", "Tyrannosaurus", "Velociraptor", "Triceratops", "Diplodocus", "Brachiosaurus", "Quetzalcoatlus", "Smilodon", "Megalonyx", "Mammuthus", "Meganeura", "Eldredgeops", "Exaeretodon", "Redondasaurus", "araucarioxylon", "Pelagiella", "Burgessia", "Naraoia", "Hallucigenia", "Pikaia", "Cameroceras", "Acanthostega", "Tulerpeton", "Pareiasaurus", "Dimetrodon", "Estemmenosuchus", "Cartorhynchus", "Ichthyosaurus", "Gracilisuchus", "Nyasasaurus", "Eoraptor", "Herrerasaurus", "Eudimorphodon", "Plesiosaurus", "Lesothosaurus", "Stegosaurus", "Kulindadromeus", "Megazostrodon", "Anchiornis", "Allosaurus", "Giraffatitan", "Teleoceras", "Diceratherium", "Nimravus", "Enhydrocyon", "Sahelanthropus"))
+  return(c("Attercopus", "Gorilla", "Panthera", "Homo", "Tyto", "Dromaius", "Aedes", "Solenopsis", "Caretta", "Crocodylus", "Solanum", "Prunus", "Rosa", "Climacograptus", "Anomalocaris", "Dunkleosteus", "Halysites", "Histiodella", "Agathiceras", "Archaeopteryx", "Juramaia", "Hylonomus", "Elginerpeton", "Rhyniognatha", "Aculeisporites", "Canadaspis", "Arandaspis", "Tyrannosaurus", "Velociraptor", "Triceratops", "Diplodocus", "Brachiosaurus", "Quetzalcoatlus", "Smilodon", "Megalonyx", "Mammuthus", "Meganeura", "Eldredgeops", "Exaeretodon", "Redondasaurus", "araucarioxylon", "Pelagiella", "Burgessia", "Naraoia", "Hallucigenia", "Pikaia", "Cameroceras", "Acanthostega", "Tulerpeton", "Pareiasaurus", "Dimetrodon", "Estemmenosuchus", "Cartorhynchus", "Ichthyosaurus", "Gracilisuchus", "Nyasasaurus", "Eoraptor", "Herrerasaurus", "Eudimorphodon", "Plesiosaurus", "Lesothosaurus", "Stegosaurus", "Kulindadromeus", "Megazostrodon", "Anchiornis", "Allosaurus", "Giraffatitan", "Teleoceras", "Diceratherium", "Nimravus", "Enhydrocyon", "Sahelanthropus", "Phidippus", "Tusoteuthis"))
 }
+#removed:
+#Plants: "Rosa","Aculeisporites","Solanum", "Prunus","araucarioxylon"
+#Dinsaurs: "Dromaius", "Velociraptor"
+#"Histiodella",
+#"Naraoia",
+
+#added:
+#Phidipuus - spider
+#Tusotheuthis - squid
 
 #' Get information on specimens from pbdb
 #'
@@ -164,6 +173,46 @@ GetTree <- function(taxa = GetTaxa(), rank="genus") {
 #plotPhylopicTreePBDB(tree = tree)
   timeTree <- paleotree::dateTaxonTreePBDB(tree)
   return(timeTree)
+}
+
+#' flips the direction of the tree at each node
+#'
+#' @param phy an object of class phylo
+#' @return a phylo object
+#'@export
+amb <- function (phy) #ambidextrous tree
+{
+    foo <- function(node, END, where) {
+        right <-  (node%%2==0) # new line
+        start <- which(phy$edge[, 1] == node)
+        end <- c(start[-1] - 1, END)
+        size <- end - start + 1
+        desc <- phy$edge[start, 2]
+        Nclade <- length(desc)
+        n <- N[desc]
+        o <- order(n, decreasing = right)
+        newpos <- c(0, cumsum(size[o][-Nclade])) + where
+        desc <- desc[o]
+        end <- end[o]
+        start <- start[o]
+        neworder[newpos] <<- start
+        for (i in 1:Nclade) if (desc[i] > nb.tip)
+            foo(desc[i], end[i], newpos[i] + 1)
+    }
+    phy <- reorder(phy)
+    nb.tip <- length(phy$tip.label)
+    nb.node <- phy$Nnode
+    nb.edge <- dim(phy$edge)[1]
+    tmp <- reorder(phy, "postorder")
+    N <- .C(node_depth, as.integer(nb.tip), as.integer(tmp$edge[,
+                                                                1]), as.integer(tmp$edge[, 2]), as.integer(nb.edge),
+            double(nb.tip + nb.node), 1L)[[5]]
+    neworder <- integer(nb.edge)
+    foo(nb.tip + 1, nb.edge, 1)
+    phy$edge <- phy$edge[neworder, ]
+    if (!is.null(phy$edge.length))
+        phy$edge.length <- phy$edge.length[neworder]
+    phy
 }
 
 # #' filter data frame produced by latong_age() exclude pbdb_data.early_interval & pbdb_data.late_interval (due to large # of NAs) and then remaining NAs (taxa with no lat long)
