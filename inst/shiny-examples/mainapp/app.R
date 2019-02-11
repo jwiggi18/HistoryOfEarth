be13e54b2f54efd0665da98c0d26e3baaa899e86
Brian OMeara <omeara.brian@gmail.com>
2019-02-11 10:16:45 -0500

    better layout

--- inst/shiny-examples/mainapp/app.R
+++ inst/shiny-examples/mainapp/app.R
@@ -1,48 +1,75 @@
-ui <- fluidPage(
-  shinydashboard::dashboardPage(
-    shinydashboard::dashboardHeader(title= "History of Earth"),
-    shinydashboard::dashboardSidebar(width = 175,
-      shinydashboard::sidebarMenu(
-        shinydashboard::menuItem(
-      # Setting id makes input$tabs give the tabName of currently-selected tab
-      #  id = "tabs",
-        selectInput("genus", "Choose an organism:",
-             choices = c("all", HistoryOfEarth::GetTaxa()),
-             multiple = FALSE, selected="all")),
+# ui <- fluidPage(
+#   shinydashboard::dashboardPage(
+#     shinydashboard::dashboardHeader(title= "History of Earth"),
+#     shinydashboard::dashboardSidebar(width = 175,
+#       shinydashboard::sidebarMenu(
+#         shinydashboard::menuItem(
+#       # Setting id makes input$tabs give the tabName of currently-selected tab
+#       #  id = "tabs",
+#         selectInput("genus", "Choose an organism:",
+#              choices = c("all", HistoryOfEarth::GetTaxa()),
+#              multiple = FALSE, selected="all")),
+# 
+#           shinydashboard::menuItem(
+#         selectInput("period", "Choose a period:",
+#              choices = c("all", HistoryOfEarth::GetAgeDF()$Period),
+#              multiple = FALSE, selected="all")))),
+# 
+#     shinydashboard::dashboardBody(
+#       tags$head(tags$style(HTML('
+#                                 /* body */
+#                                 .content-wrapper, .right-side {
+#                                   background-color: #000000;
+#                                   color: #ffffff
+#                                 }
+#                                 '))),
+# 
+#         fluidRow(
+# 
+#           column(5,
+#             h4("Tree"),
+#             plotOutput("tree")),
+# 
+#             column(7,
+#               textOutput("period_name"),
+#             #  h4("Map"),
+#               plotOutput("map"),
+#               h4("Picture"),
+#               uiOutput("img"))
+#       ) 
+# 
+#     )
+# 
+#   )
+# )
 
-          shinydashboard::menuItem(
-        selectInput("period", "Choose a period:",
-             choices = c("all", HistoryOfEarth::GetAgeDF()$Period),
-             multiple = FALSE, selected="all")))),
-
-    shinydashboard::dashboardBody(
-      tags$head(tags$style(HTML('
-                                /* body */
-                                .content-wrapper, .right-side {
-                                  background-color: #000000;
-                                  color: #ffffff
-                                }
-                                '))),
-
-        fluidRow(
-
-          column(5,
-            h4("Tree"),
-            plotOutput("tree")),
-
-            column(7,
-              textOutput("period_name"),
-            #  h4("Map"),
-              plotOutput("map"),
-              h4("Picture"),
-              uiOutput("img"))
-      ) 
-
-    )
-
-  )
+ui <- fluidPage(theme = shinythemes::shinytheme("cyborg"),
+    fluidRow(
+        column(6,
+            selectInput("genus", "Choose an organism:",
+                choices = c("all", HistoryOfEarth::GetTaxa()),
+                multiple = FALSE, selected="all")
+        ),
+        column(6,
+            selectInput("period", "Choose a period:",
+                choices = c("all", HistoryOfEarth::GetAgeDF()$Period),
+                multiple = FALSE, selected="all")
+        )
+    ),
+    fluidRow(
+        column(6,
+               textOutput("period_name"),
+               plotOutput("map")
+        ),
+        column(6,
+               uiOutput("img")
+        )
+    ),
+    fluidRow(plotOutput("tree"))
 )
-
+    
+    
+     
 
 server <- function(input, output) {
   library(HistoryOfEarth)
@@ -79,7 +106,7 @@ chosen_period <- reactive({input$period})
   }, deleteFile = FALSE)
 
   output$img <- renderUI({
-      tags$img(src=HistoryOfEarth::GetWikipediaThumbnail(input$genus))
+      tags$img(src=HistoryOfEarth::GetWikipediaThumbnail(input$genus), width='50%')
      # list(src="https://upload.wikimedia.org/wikipedia/commons/c/c1/La_Brea_Tar_Pits.jpg")
   })
 
